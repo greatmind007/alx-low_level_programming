942d1e021cbdad6af95a8ecaf616a5f24156cf43
greatmind007 <dapo2sammy@gmail.com>
2022-10-15 16:10:20 -0700

    0x01.c-variables, if, else, while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main-program entry point.
+  * Return: 0 no error, non zero if error
+  **/
+int main(void)
+{
+	char i;
+	char j;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
+}
